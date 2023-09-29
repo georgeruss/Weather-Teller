24d39a71354fe296e9698a90dc99d3e8df628cb7
georgeruss <landynrussell2019@gmail.com>
2023-09-29 12:00:41 -0400

    commits for weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Search from './components/search/search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://youtube.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello.
-        </a>
-      </header>
+    <div className="container">
+      <Search />
     </div>
   );
 }
---
+++ src/components/search/search.js
@@ -0,0 +1,12 @@
+import { AsyncPaginate } from "react-select-async-paginate";
+const Search = () => {
+    return (
+        <AsyncPaginate 
+            placeholder="Search for city"
+            debounceTimeout={}
+        />
+    )
+
+}
+
+export default Search;
